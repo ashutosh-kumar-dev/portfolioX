1029cb9510e1daed04402cfbfcfa77dbce60fa1c
ashutosh kumar <turboblitz.dev@gmail.com>
2025-05-08 12:14:19 +0530

    fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Routes, Route} from "react-router-dom";
 import Navigation from "./components/navBar/Navigation";
+// import Navigation from "../saved_templates/Navigation";
+import ProfileCard from "./components/profileCard/ProfileCard";
+import LeetCodeHeatmap  from "./components/about/LeetCodeHeatmap";
 import About from "./components/About";
+import TimelineExperience from "./components/timeline/TimelineExperience";
+import TimelineProjects from "./components/timeline/TimelineProjects";
+import SkillShowcase from "./components/skill/SkillShowcase";
 import Resume from "./components/Resume";
 import Projects from "./components/projects/Projects";
-import Education from "./components/education/Education";
 import PageRoutes from './components/PageRoutes';
 
 function App() {
@@ -18,15 +23,6 @@ function App() {
 
       {/* Scrollable Content */}
       <div className="mt-[4.4rem]  ">
-        <Routes>
-          <Route path="/" element={<About />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/education" element={<Education />} />
-          {/* Add a fallback route */}
-          <Route path="*" element={<div>Page Not Found</div>} />
-        </Routes>
         <PageRoutes />
       </div>
     </div>
