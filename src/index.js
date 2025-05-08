5f3aeb32d8571d169f5cf11f55e43c740aa0f965
ashutosh kumar <turboblitz.dev@gmail.com>
2025-05-08 12:22:49 +0530

    fixed scrolling

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css"; // Ensure your global styles are imported
+import ScrollToTop from "./components/ScrollToTop";
 
 ReactDOM.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>,
   document.getElementById("root")
